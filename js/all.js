ad03cb8ddcb9a830dff624655704884ba4ed68b3
Angelo Semeraro <angeloseme@gmail.com>
2016-06-09 23:51:06 +0200

    Adding all.html

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-var $all=$('<sections></sections>');
+
 
 $(document).ready(function(){
   function parse(i){
@@ -12,14 +12,14 @@ $(document).ready(function(){
         success: function(xml){
           $("#bulletin-container").append('<div style="display:block;width:100%;background-color:black;color:white;">'+paths[i]+'</div>');
           console.log('processing: '+paths[i]);
-        //  processXML(xml);
-          $all.append(xml.find('sections'));
+          processXML(xml);
+        //  $all.append(xml.find('sections'));
           parse(i+1);
         }
       });
     }
     else{
-      processXML($all);
+    //  processXML($all);
     }
   }
   parse(0);
